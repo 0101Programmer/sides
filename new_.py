e149029cba7dedcb9b6de3e7753e1e1da3d6a75c
0101Programmer <baranov-v-k@yandex.ru>
2024-08-17 07:20:08 +0300

    Add files via upload

---
+++ new_.py
@@ -0,0 +1,31 @@
+def __is_valid_sides__(l1, l2, l3, *args):
+    list_ = l1, l2, l3
+    if len(args) == len(list_):
+        if args[0] > 0 and args[0] % 1 == 0 and args[1] > 0 and args[1] % 1 == 0 and args[2] > 0 and args[2] % 1 == 0:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+print(__is_valid_sides__(1, 3, 10, 1, 2, 3))
+print(__is_valid_sides__(1, 3, 10, 1, 2.5, 3))
+
+
+def __is_valid_sides__2(l1, l2, l3, *args):
+    list_ = l1, l2, l3
+    if len(args) == len(list_):
+        for i in args:
+            if i > 0 and i % 1 == 0:
+                return True
+            else:
+                return False
+    else:
+        return False
+
+
+print(__is_valid_sides__2(1, 3, 10, 1, 2, 3))
+print(__is_valid_sides__2(1, 3, 10, 1, 2.5, 3))
+
+
